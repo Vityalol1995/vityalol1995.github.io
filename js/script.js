de0cb7be5483e5c10d62937f9ecf793bc6288cfb
vitya1995 <51419053+Vityalol1995@users.noreply.github.com>
2020-01-19 22:11:16 +0200

    qwery

---
+++ js/script.js
@@ -0,0 +1,86 @@
+$(function() {
+    
+     var header = $("#header"),
+     introH = $("#intro").innerHeight(),
+     scrollOffset = $(window).scrollTop();
+    
+    /*Fixed Header*/
+     checkscroll(scrollOffset);
+    
+   $(window).on("scroll", function() {
+    
+       scrollOffset = $(this).scrollTop();
+       
+       checkscroll(scrollOffset);
+   }); 
+    
+    function checkscroll(scrollOffset) {
+       
+       if( scrollOffset >= introH ) {
+           header.addClass("fixed"); 
+       } else {
+           header.removeClass("fixed");
+       } 
+    }
+    
+    
+    /*Smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    
+    /*Menu Nav toggle*/
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+        
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+    
+    
+    /*Collapce*/
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this),
+            blockId = $this.data('collapse');
+        
+        $this.toggleClass("active")
+        
+    });
+    
+    /*Slider*/
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
